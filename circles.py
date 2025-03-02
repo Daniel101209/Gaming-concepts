af78d3e48e5ccd18c5ce83aacb10fb72f2708519
Daniel101209 <daniel.shumilov29@mva.school>
2025-03-02 11:50:46 +0200

    Add files via upload

---
+++ circles.py
@@ -0,0 +1,12 @@
+import pgzrun
+from random import randint
+
+WIDTH = 300
+HEIGHT = 300
+
+def draw():
+    screen.fill("black")
+    screen.draw.circle((150,150),100,"red")
+    screen.draw.circle((150,150),90,"sky blue")
+pgzrun.go()
+    
