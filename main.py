52f2288abd41247c823eb2bbd5d7c3891700facc
Daniel101209 <daniel.shumilov29@mva.school>
2025-05-17 13:29:49 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,58 +1,107 @@
 import pgzrun
-from random import randint
-from time import time
+import random
 
 WIDTH=800
-HEIGHT=470
+HEIGHT=600
+CENTRE_X = WIDTH//2
+CENTRE_Y = HEIGHT//2
+CENTRE = (CENTRE_X,CENTRE_Y)
 
-eggs=[]
-lines = []
-next_egg = 0
 
-start_time=0
-total_time=0
-end_time=0
+FINAL_LEVEL = 6
+START_SPEED = 10
+ITEMS=["plasticbag","battery","bottle","paperbag","packetofchips"]
 
-number_of_eggs=8
-
-def create_eggs():
-    global start_time
-    for i in range(0,number_of_eggs):
-        egg=Actor("egg")
-        egg.pos=randint(40,WIDTH-40),randint(40,HEIGHT-40)
-        eggs.append(egg)
-    start_time = time()
+game_over = False
+game_complete = False
+current_level=1
+items=[]
+animations=[]
 
 def draw():
-    global total_time
-    screen.blit("bgforegg",(0,0))
-    num=1
-    for s in eggs:
-        screen.draw.text(str(num),(s.pos[0], s.pos[1]+20))
-        s.draw()
-        num=num+1
-    for line in lines:
-        screen.draw.line(line[0],line[1],"red")
-    if next_egg < number_of_eggs:
-        total_time=time()-start_time
-        screen.draw.text(str(round(total_time,1)),(10,10),fontsize=30)
+    global items,current_level,game_complete,game_over
+    screen.clear()
+    screen.blit("background",(0,0))
+    if game_over:
+        display_message("GAME OVER","Try again")
+    elif game_complete:
+        display_message("You won","Well done")
     else:
-        screen.draw.text(str(round(total_time,1)),(10,10),fontsize=30)
+        for item in items:
+            item.draw()
 
 def update():
-    pass
+    global items
+    if len(items) == 0:
+        items=make_items(current_level)
+
+def make_items(number_of_extra_items):
+    items_to_create=get_option_to_create(number_of_extra_items)
+    new_items=create_items(items_to_create)
+    layout_items(new_items)
+    animate_items(new_items)
+    return new_items
+
+def get_option_to_create(number_of_extra_items):
+    item_to_create=["paperbag"]
+    for i in range(0,number_of_extra_items):
+        random_option=random.choice(ITEMS)
+        item_to_create.append(random_option)
+    return item_to_create
+
+def create_items(items_to_create):
+    new_items=[]
+    for option in items_to_create:
+        item=Actor(option)
+        new_items.append(item)
+    return new_items
+
+def layout_items(items_to_layout):
+    number_of_gaps= len(items_to_layout) + 1
+    gap_size=WIDTH/number_of_gaps
+    random.shuffle(items_to_layout)
+    for index,item in enumerate(items_to_layout):
+        new_x_pos = (index + 1)* gap_size
+        item.x = new_x_pos
+
+def animate_items(items_to_animate):
+    global animations
+    for item in items_to_animate:
+        duration=START_SPEED- current_level
+        item.anchor=("center","bottom")
+        animation = animate(item,duration=duration,on_finished=handle_game_over,y=HEIGHT)
+        animations.append(animation)
+
+def handle_game_over():
+    global game_over
+    game_over=True
 
 def on_mouse_down(pos):
-    global next_egg, lines
-    if next_egg < number_of_eggs:
-        if eggs[next_egg].collidepoint(pos):
-            if next_egg:
-                lines.append((eggs[next_egg-1].pos,eggs[next_egg].pos))
-            next_egg=next_egg+1
-        else:
-            lines=[]
-            next_egg
+    global items, current_level
+    for item in items:
+        if item.collidepoint(pos):
+            if "paperbag" in item.image:
+                handle_game_complete()
+            else:
+                handle_game_over()
 
+def handle_game_complete():
+    global current_level,items,animations, game_complete
+    stop_animations(animations)
+    if current_level == FINAL_LEVEL:
+        game_complete=True
+    else:
+        current_level = current_level+1
+        items=[]
+        animations=[]
 
-create_eggs()
+def stop_animations(animations_to_stop):
+    for animation in animations_to_stop:
+        if animation.running:
+            animation.stop()
+
+def display_message(heading_text, sub_heading_text):
+    screen.draw.text(heading_text,fontsize=60,center=CENTRE,color="white")
+    screen.draw.text(sub_heading_text,fontsize=30,center=(CENTRE_X,CENTRE_Y+30),color="white")
+
 pgzrun.go()
