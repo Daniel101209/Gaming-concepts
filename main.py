66eda48b1c049d1f52c34f1e2b6f45a835692847
Daniel101209 <daniel.shumilov29@mva.school>
2025-06-08 11:11:08 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -5,19 +5,19 @@ WIDTH = 1200
 HEIGHT = 600
 
 WHITE=(255,255,255)
-BLUE=(0,0,255)
+BLUE=(255,0,0)
 
-ship= Actor('galaga')
-bug=Actor('bug')
+main_character= Actor('emmet')
+villan=Actor('robot')
 
-ship.pos=(WIDTH//2, HEIGHT-60)
+main_character.pos=(WIDTH//2, HEIGHT-60)
 
 speed=5
 
 bullets=[]
 enemies=[]
 
-enemies.append(Actor('bug'))
+enemies.append(Actor('robot'))
 enemies[-1].x=10
 enemies[-1].y=-100
 
@@ -28,31 +28,31 @@ def displayScore():
 
 def on_key_down(key):
     if key == keys.SPACE:
-        bullets.append(Actor('bullet'))
-        bullets[-1].x=ship.x
-        bullets[-1].y=ship.y - 50
+        bullets.append(Actor('stud'))
+        bullets[-1].x=main_character.x
+        bullets[-1].y=main_character.y - 50
 
 def update():
     global score
     if keyboard.left:
-        ship.x -= speed
-        if ship.x <=0:
-            ship.x=0
+        main_character.x -= speed
+        if main_character.x <=0:
+            main_character.x=0
     elif keyboard.right:
-        ship.x += speed
-        if ship.x >= 1200:
-            ship.x=1200
+        main_character.x += speed
+        if main_character.x >= 1200:
+            main_character.x=1200
     if keyboard.space:
         print("Pressing space")
-        bullets.append(Actor('bullet'))
-        bullets[-1].x=ship.x
-        bullets[-1].y=ship.y
+        bullets.append(Actor('stud'))
+        bullets[-1].x=main_character.x
+        bullets[-1].y=main_character.y
 
-    for bullet in bullets:
-        if bullet.y <=0:
-            bullets.remove(bullet)
+    for stud in bullets:
+        if stud.y <=0:
+            bullets.remove(stud)
         else:
-            bullet.y -=10
+            stud.y -=10
     
     for enemy in enemies:
         enemy.y +=5
@@ -61,10 +61,10 @@ def update():
             enemy.y= -100
             enemy.x=random.randint(50,WIDTH-50)
 
-        for bullet in bullets:
-            if enemy.colliderect(bullet):
+        for stud in bullets:
+            if enemy.colliderect(stud):
                 score=score+100
-                bullets.remove(bullet)
+                bullets.remove(stud)
                 enemies.remove(enemy)
 
 def draw():
@@ -74,7 +74,6 @@ def draw():
         bullet.draw()
     for enemy in enemies:
         enemy.draw()
-    ship.draw()
     displayScore()
 
 pgzrun.go()
