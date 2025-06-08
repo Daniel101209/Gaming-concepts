c363caa4612f2690deeedb0a4b4211cb90554041
Daniel101209 <daniel.shumilov29@mva.school>
2025-06-08 11:47:03 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,72 +1,81 @@
+#importing the required modules
 import pgzrun
 import random
-
+#setting up the size of the screen
 WIDTH = 1200
 HEIGHT = 600
-
+#Setting up the colors
 WHITE=(255,255,255)
-BLUE=(255,0,0)
-
-main_character= Actor('emmet')
-villan=Actor('robot')
-
-main_character.pos=(WIDTH//2, HEIGHT-60)
+BLUE=(0,0,255)
+#Importing the charachters
+ship= Actor('galaga')
+bug=Actor('bug')
+#Setting the position and speed of the ship
+ship.pos=(WIDTH//2, HEIGHT-60)
 
 speed=5
-
+#Creating the required lists
 bullets=[]
 enemies=[]
-
-enemies.append(Actor('robot'))
-enemies[-1].x=10
-enemies[-1].y=-100
+#Creating the nymber if enemies
+for x in range(8):
+    enemies.append(Actor('bug'))
+    enemies[-1].x=100 + 90*x
+enemies[-1].y= 80
 
 score=0
-
+direction=1
+#Displaying the score
 def displayScore():
     screen.draw.text(str(score),(50,30))
 
+#creating the functions for updating and pressing on keys 
 def on_key_down(key):
     if key == keys.SPACE:
-        bullets.append(Actor('stud'))
-        bullets[-1].x=main_character.x
-        bullets[-1].y=main_character.y - 50
+        bullets.append(Actor('bullet'))
+        bullets[-1].x=ship.x
+        bullets[-1].y=ship.y - 50
 
 def update():
     global score
+    global direction
+    moveDown= False
+
     if keyboard.left:
-        main_character.x -= speed
-        if main_character.x <=0:
-            main_character.x=0
+        ship.x -= speed
+        if ship.x <=0:
+            ship.x=0
     elif keyboard.right:
-        main_character.x += speed
-        if main_character.x >= 1200:
-            main_character.x=1200
+        ship.x += speed
+        if ship.x >= 1200:
+            ship.x=1200
+    
     if keyboard.space:
         print("Pressing space")
-        bullets.append(Actor('stud'))
-        bullets[-1].x=main_character.x
-        bullets[-1].y=main_character.y
-
-    for stud in bullets:
-        if stud.y <=0:
-            bullets.remove(stud)
+        bullets.append(Actor('bullet'))
+        bullets[-1].x=ship.x
+        bullets[-1].y=ship.y
+#Creating a function for moving/deleting bullets
+    for bullet in bullets:
+        if bullet.y <=0:
+            bullets.remove(bullet)
         else:
-            stud.y -=10
-    
+            bullet.y -=10
+#Making the enemies move in a row    
+    if len (enemies) > 0 and (enemies[-1].x > WIDTH -80 or enemies[0].x < 80):
+        moveDown=True
+        direction= direction*-1
+
     for enemy in enemies:
-        enemy.y +=5
-
-        if enemy.y >= HEIGHT:
-            enemy.y= -100
-            enemy.x=random.randint(50,WIDTH-50)
-
-        for stud in bullets:
-            if enemy.colliderect(stud):
+        enemy.x += 5* direction
+        if moveDown == True:
+            enemy.y += 50
+#Making so the bullet gets deleted if it touches the enemy
+        for bullet in bullets:
+            if enemy.colliderect(bullet):
                 score=score+100
-                bullets.remove(stud)
-                enemies.remove(enemy)
-
+                bullets.remove(bullet)
+#Drawing everything on the screen
 def draw():
     screen.clear()
     screen.fill(BLUE)
@@ -74,6 +83,8 @@ def draw():
         bullet.draw()
     for enemy in enemies:
         enemy.draw()
+    ship.draw()
     displayScore()
+
 
 pgzrun.go()
