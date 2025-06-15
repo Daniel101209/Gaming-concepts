14a51c85404401c439724a2b40795f417f39fe8c
Daniel101209 <daniel.shumilov29@mva.school>
2025-06-15 11:58:00 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,88 +1,26 @@
-import pgzrun
 import random
+import itertools
+import pgzrun
 
-WIDTH = 1200
-HEIGHT = 600
+WIDTH = 400
+HEIGHT = 400
 
-WHITE=(255,255,255)
-RED=(255,0,0)
+BLOCK_POSITIONS = [(350,50),(350,350),(50,350),(50,50)]
 
-main_character= Actor('emmet')
-robot=Actor('robot')
+block_positions=itertools.cycle(BLOCK_POSITIONS)
 
-main_character.pos=(WIDTH//2, HEIGHT-60)
-
-speed=5
-
-bullets=[]
-enemies=[]
-
-for x in range(8):
-    enemies.append(Actor('robot'))
-    enemies[-1].x=100 + 90*x
-enemies[-1].y= 80
-
-score=0
-direction=1
-
-def displayScore():
-    screen.draw.text(str(score),(50,30))
- 
-def on_key_down(key):
-    if key == keys.SPACE:
-        bullets.append(Actor('stud'))
-        bullets[-1].x=main_character.x
-        bullets[-1].y=main_character.y - 50
-
-def update():
-    global score
-    global direction
-    moveDown= False
-
-    if keyboard.left:
-        main_character.x -= speed
-        if main_character.x <=0:
-            main_character.x=0
-    elif keyboard.right:
-        main_character.x += speed
-        if main_character.x >= 1200:
-            main_character.x=1200
-    
-    if keyboard.space:
-        print("Pressing space")
-        bullets.append(Actor('stud'))
-        bullets[-1].x=main_character.x
-        bullets[-1].y=main_character.y
-
-    for bullet in bullets:
-        if bullet.y <=0:
-            bullets.remove(bullet)
-        else:
-            bullet.y -=10
-    
-    if len (enemies) > 0 and (enemies[-1].x > WIDTH -80 or enemies[0].x < 80):
-        moveDown=True
-        direction= direction*-1
-
-    for enemy in enemies:
-        enemy.x += 5* direction
-        if moveDown == True:
-            enemy.y += 50
-
-        for bullet in bullets:
-            if enemy.colliderect(bullet):
-                score=score+100
-                bullets.remove(bullet)
+block=Actor('block',center=(50,50))
+ship=Actor('ship',center=(200,200))
 
 def draw():
     screen.clear()
-    screen.fill(RED)
-    for bullet in bullets:
-        bullet.draw()
-    for enemy in enemies:
-        enemy.draw()
-    main_character.draw()
-    displayScore()
+    block.draw()
+    ship.draw()
 
+def move_block():
+    animate(block,'bounce_end',duration=1,pos=next(block_positions))
+
+move_block()
+clock.schedule_interval(move_block,2)
 
 pgzrun.go()
