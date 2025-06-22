1a54521e74c8093fde63ed0289897210102560b2
Daniel101209 <daniel.shumilov29@mva.school>
2025-06-22 11:07:02 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -5,22 +5,22 @@ import pgzrun
 WIDTH = 400
 HEIGHT = 400
 
-BLOCK_POSITIONS = [(350,50),(350,350),(50,350),(50,50)]
+BALL_POSITIONS = [(350,50),(350,350),(50,350),(50,50)]
 
-block_positions=itertools.cycle(BLOCK_POSITIONS)
+block_positions=itertools.cycle(BALL_POSITIONS)
 
-block=Actor('block',center=(50,50))
+ball=Actor('ball',center=(50,50))
 ship=Actor('ship',center=(200,200))
 
 def draw():
     screen.clear()
-    block.draw()
+    ball.draw()
     ship.draw()
 
-def move_block():
-    animate(block,'bounce_end',duration=1,pos=next(block_positions))
+def move_ball():
+    animate(ball,'bounce_end',duration=1,pos=next(block_positions))
 
-move_block()
-clock.schedule_interval(move_block,2)
+move_ball()
+clock.schedule_interval(move_ball,2)
 
 pgzrun.go()
