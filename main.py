932725c40579f051f2fd64052d0a023b45ec7b88
Daniel101209 <daniel.shumilov29@mva.school>
2025-06-22 11:28:32 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -5,22 +5,46 @@ import pgzrun
 WIDTH = 400
 HEIGHT = 400
 
-BALL_POSITIONS = [(350,50),(350,350),(50,350),(50,50)]
+BLOCK_POSITIONS = [(350,50),(350,350),(50,350),(50,50)]
 
-block_positions=itertools.cycle(BALL_POSITIONS)
+block_positions=itertools.cycle(BLOCK_POSITIONS)
 
-ball=Actor('ball',center=(50,50))
+block=Actor('block',center=(50,50))
 ship=Actor('ship',center=(200,200))
 
 def draw():
     screen.clear()
-    ball.draw()
+    block.draw()
     ship.draw()
 
-def move_ball():
-    animate(ball,'bounce_end',duration=1,pos=next(block_positions))
+def move_block():
+    animate(block,'bounce_end',duration=1,pos=next(block_positions))
 
-move_ball()
-clock.schedule_interval(move_ball,2)
+move_block()
+clock.schedule_interval(move_block,2)
 
+def next_ship_target():
+    x=random.randint(50,350)
+    y=random.randint(50,350)
+    ship.target=x,y
+
+    target_angle=ship.angle_to(ship.target)
+
+    animate(
+        ship,
+        angle=target_angle,
+        duration=0.3,
+        on_finished=move_ship
+    )
+
+def move_ship():
+    anim=animate(
+        ship,
+        tween="accel_decel",
+        pos=ship.target,
+        duration=ship.distance_to(ship.target)/200,
+        on_finished=next_ship_target
+    )
+
+next_ship_target()
 pgzrun.go()
