d7b45f13f24cea7bf2210146f89571117ebf6efe
Daniel101209 <daniel.shumilov29@mva.school>
2025-06-29 11:53:31 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,50 +1,23 @@
-import random
-import itertools
-import pgzrun
+import pygame
 
-WIDTH = 400
-HEIGHT = 400
+pygame.init()
 
-BLOCK_POSITIONS = [(350,50),(350,350),(50,350),(50,50)]
+screen=pygame.display.set_mode((800,600))
+pygame.display.set_caption("Understanding OOP")
 
-block_positions=itertools.cycle(BLOCK_POSITIONS)
+class Rect():
+    def __init__(self,color,dimensions):
+        self.place=screen
+        self.c=color
+        self.dim=dimensions
 
-block=Actor('block',center=(50,50))
-ship=Actor('ship',center=(200,200))
+    def draw(self):
+        self.draw_rect=pygame.draw.rect(self.place,self.c,self.dim)
 
-def draw():
-    screen.clear()
-    block.draw()
-    ship.draw()
+r1=Rect("red",(90,30,300,300))
+r1.draw()
 
-def move_block():
-    animate(block,'bounce_end',duration=1,pos=next(block_positions))
+r2=Rect("blue",(40,120,0,0))
+r2.draw()
 
-move_block()
-clock.schedule_interval(move_block,2)
-
-def next_ship_target():
-    x=random.randint(50,350)
-    y=random.randint(50,350)
-    ship.target=x,y
-
-    target_angle=ship.angle_to(ship.target)
-
-    animate(
-        ship,
-        angle=target_angle,
-        duration=0.3,
-        on_finished=move_ship
-    )
-
-def move_ship():
-    anim=animate(
-        ship,
-        tween="accel_decel",
-        pos=ship.target,
-        duration=ship.distance_to(ship.target)/200,
-        on_finished=next_ship_target
-    )
-
-next_ship_target()
-pgzrun.go()
+pygame.display.update()
