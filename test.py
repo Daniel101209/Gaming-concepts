733747aaf2701735491d8cb8a250416dc1975b59
Daniel101209 <daniel.shumilov29@mva.school>
2025-06-29 11:25:34 +0300

    Add files via upload

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 import pgzrun
+from random import randint
 
 WIDTH=400
 HEIGHT=400
@@ -7,8 +8,34 @@ witch=Actor('witch')
 witch.pos=200,200
 poition=Actor('poition')
 poition.pos=100,200
+
+
 def draw():   
     screen.blit('night forest',(0,0))
     witch.draw()
     poition.draw()
+
+def update():
+    if keyboard.up:
+        witch.y = witch.y - 3
+    if keyboard.down:
+        witch.y = witch.y + 3
+    if keyboard.right:
+        witch.x = witch.x + 3
+    if keyboard.left:
+        witch.x = witch.x - 3
+
+def select_target():
+    global position
+    target_x = randint(50,WIDTH-50)
+    target_y = randint(50,HEIGHT-50)
+    position=(target_x,target_y)
+
+def move_poition():
+    global position
+    if witch.colliderect(poition):
+        animate(poition,"bounce_end",duration=1,pos=next(position))
+
+move_poition()
+update()
 pgzrun.go()
