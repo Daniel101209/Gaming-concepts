0ade60b9a8f134f7afd2fd2e77bed7ae43357e1e
Daniel101209 <daniel.shumilov29@mva.school>
2025-07-27 11:48:49 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,52 +1,66 @@
 import pygame
-import time
+from pygame.locals import *
+from time import *
 
 pygame.init()
 
-WIDTH=800
-HEIGHT=600
+screen=pygame.display.set_mode((600,600))
 
-screen=pygame.display.set_mode((WIDTH,HEIGHT))
-pygame.display.set_caption("Father's day Greeting card")
+player_x=200
+player_y=200
 
-image=pygame.image.load("C:/Users/User/Desktop/gaming concepts/PRO GAME DEV/father's day/images/father's day card 1.jpg")
-image=pygame.transform.scale(image,(WIDTH,HEIGHT))
+keys=[False,False,False,False]
 
-gameloop = True
-while (gameloop):
+player=pygame.image.load("C:/Users/User/Desktop/gaming concepts/PRO GAME DEV/rocket in space/Rocket.png")
+player=pygame.transform.scale(player,(100,100))
+background=pygame.image.load("C:/Users/User/Desktop/gaming concepts/PRO GAME DEV/rocket in space/galaxy.jpg")
+background=pygame.transform.scale(background,(600,600))
+
+while player_y < 600:
+    screen.blit(background,(0,0))
+    screen.blit(player,(player_x,player_y))
+    pygame.display.flip()
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            gameloop=False
+            pygame.quit()
+            exit(0)
+
+    if event.type == pygame.KEYDOWN:
+        if event.key == K_UP:
+            keys[0]=True
+        elif event.key == K_LEFT:
+            keys[1]=True
+        elif event.key == K_DOWN:
+            keys[2]=True
+        if event.key == K_RIGHT:
+            keys[3]=True
+
+    if event.type == pygame.KEYUP:
+        if event.key == pygame.K_UP:
+            keys[0]=False
+        elif event.key == pygame.K_LEFT:
+            keys[1]=False
+        elif event.key == pygame.K_DOWN:
+            keys[2]=False
+        if event.key == pygame.K_RIGHT:
+            keys[3]=False
+
+    if keys[0]:
+        if player_y > 0:
+            player_y -= 7
+    elif keys[2]:
+        if player_y < 550:
+            player_y += 7
+
+    if keys[1]:
+        if player_x > 0:
+            player_x -= 2
+    elif keys[3]:
+        if player_x < 550:
+            player_x += 2
     
-    font=pygame.font.SysFont("Comic Sans MS",50)
-    font1=pygame.font.SysFont("Comic Sans MS",30)
-    t1=font.render("Happy",True,"red")
-    t2=font.render("Father's day!",True,"red")
-    screen.blit(image,(0,0))
-    screen.blit(t1,(280,250))
-    screen.blit(t2,(280,330))
-    pygame.display.update()
-    time.sleep(2)
+    player_y += 5
+    sleep(0.05)
 
-    image2=pygame.image.load("C:/Users/User/Desktop/gaming concepts/PRO GAME DEV/father's day/images/father's day card 2.jpg")
-    image2=pygame.transform.scale(image2,(WIDTH,HEIGHT))
-
-    t3=font.render("To the best father in the world!",True,"red")
-    screen.fill("white")
-    screen.blit(image2,(0,0))
-    screen.blit(t3,(30,200))
-    pygame.display.update()
-    time.sleep(2)
-
-    image3=pygame.image.load("C:/Users/User/Desktop/gaming concepts/PRO GAME DEV/father's day/images/father's day card 3.jpg")
-    image3=pygame.transform.scale(image3,(WIDTH,HEIGHT))
-
-    t4=font1.render("Wish you always be the same and amazing as you are!",True,"red")
-    screen.fill("white")
-    screen.blit(image3,(0,0))
-    screen.blit(t4,(30,200))
-    pygame.display.update()
-    time.sleep(2)
- 
-pygame.quit()
+print("Game over!")
