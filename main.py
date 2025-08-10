b035f3237cb95eb9e2796d517f95c6c41db35e2a
Daniel101209 <daniel.shumilov29@mva.school>
2025-08-10 12:02:06 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -12,6 +12,17 @@ x= 450
 player_ship=pygame.image.load("C:/Users/User/Desktop/gaming concepts/PRO GAME DEV/space game/spaceship.png")
 player_ship=pygame.transform.scale(player_ship,(100,100))
 
+x1= 450
+spaceship2 = pygame.image.load("C:/Users/User/Desktop/gaming concepts/PRO GAME DEV/space game/spaceship2.png")
+spaceship2=pygame.transform.rotate(spaceship2,180)
+spaceship2=pygame.transform.scale(spaceship2,(100,100))
+spaceship2_rect=spaceship2.get_rect(center=(x1,10))
+
+bullet=pygame.image.load("C:/Users/User/Desktop/gaming concepts/PRO GAME DEV/space game/bullet.png")
+bullet=pygame.transform.scale(bullet,(10,10))
+
+bullets = []
+
 gameloop=True
 FPS=60
 clock=pygame.time.Clock()
@@ -22,14 +33,29 @@ while gameloop:
             gameloop = False
 
     if event.type == pygame.KEYDOWN:
-        if event.key == pygame.K_LEFT:
+        if event.key == pygame.K_LEFT and x > 0:
             x=x-3
-        elif event.key == pygame.K_RIGHT:
+        elif event.key == pygame.K_RIGHT and x < 800:
             x=x+3
+        
+    keys=pygame.key.get_pressed()
+    if keys[pygame.K_a] and x1 > 0:
+        x1=x1-3
+    if keys[pygame.K_d] and x1 < 800:
+        x1=x1+3
+
+    if keys[pygame.K_SPACE]:
+        bullet_rect=bullet.get_rect(center=spaceship2_rect.midtop)
+        bullets.append({"rect":bullet_rect})
+    for b in bullets:
+        b["rect"].y=b["rect"].y+5
+
 
     screen.blit(background,(0,0))
     screen.blit(player_ship,(x,400))
-
+    screen.blit(spaceship2,(x1,10))
+    for b in bullets:
+        screen.blit(bullet,b["rect"])
     pygame.display.flip()
     clock.tick(FPS)
 
