86c444a234ba60621dc3420e4cfdd2e0d92612da
Daniel101209 <daniel.shumilov29@mva.school>
2025-08-24 12:03:08 +0300

    Add files via upload

---
+++ match the following/main.py
@@ -0,0 +1,54 @@
+import pygame
+
+pygame.init()
+
+screen=pygame.display.set_mode((600,600))
+pygame.display.set_caption("Match it!")
+
+subway_surfers=pygame.image.load("C:/Users/User/Desktop/gaming concepts/PRO GAME DEV/match the following/subway surfers.png")
+candy_crush=pygame.image.load("C:/Users/User/Desktop/gaming concepts/PRO GAME DEV/match the following/candy crush.png")
+cut_the_rope=pygame.image.load("C:/Users/User/Desktop/gaming concepts/PRO GAME DEV/match the following/cut the rope.png")
+angry_birds=pygame.image.load("C:/Users/User/Desktop/gaming concepts/PRO GAME DEV/match the following/angry birds.png")
+
+subway_surfers = pygame.transform.scale(subway_surfers,(100,100))
+candy_crush = pygame.transform.scale(candy_crush,(100,100))
+cut_the_rope = pygame.transform.scale(cut_the_rope,(100,100))
+angry_birds = pygame.transform.scale(angry_birds,(100,100))
+
+f=pygame.font.SysFont("Comic Sans MS",36)
+
+text1=f.render("Subway Surfers",True,"white")
+text2=f.render("Candy Crush",True,"white")
+text3=f.render("Cut The Rope",True,"white")
+text4=f.render("Angry Birds",True,"white")
+
+gameloop = True
+
+while gameloop:
+    screen.fill("Black")
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            gameloop=False
+
+        screen.blit(subway_surfers,(150,205))
+        screen.blit(candy_crush,(150,5))
+        screen.blit(cut_the_rope,(150,305))
+        screen.blit(angry_birds,(150,105))
+
+        screen.blit(text1,(300,50))
+        screen.blit(text2,(300,150))
+        screen.blit(text3,(300,250))
+        screen.blit(text4,(300,350))
+
+    
+        if event.type==pygame.MOUSEBUTTONDOWN:
+            pos=pygame.mouse.get_pos()
+            pygame.draw.circle(screen,"cyan",(pos),20,0)
+            pygame.display.update()
+        elif event.type == pygame.MOUSEBUTTONUP:
+            pos2=pygame.mouse.get_pos()
+            pygame.draw.line(screen,"hotpink",(pos),(pos2),5)
+            pygame.draw.circle(screen,"cyan",(pos2),20,0)
+            pygame.display.update()
+        pygame.display.update()
+pygame.quit()
