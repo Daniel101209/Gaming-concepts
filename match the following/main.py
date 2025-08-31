d217e5f4af240663edb0cae90821601c0f94bfcc
Daniel101209 <daniel.shumilov29@mva.school>
2025-08-31 11:14:10 +0300

    Add files via upload

--- match the following/main.py
+++ match the following/main.py
@@ -9,11 +9,15 @@ subway_surfers=pygame.image.load("C:/Users/User/Desktop/gaming concepts/PRO GAME
 candy_crush=pygame.image.load("C:/Users/User/Desktop/gaming concepts/PRO GAME DEV/match the following/candy crush.png")
 cut_the_rope=pygame.image.load("C:/Users/User/Desktop/gaming concepts/PRO GAME DEV/match the following/cut the rope.png")
 angry_birds=pygame.image.load("C:/Users/User/Desktop/gaming concepts/PRO GAME DEV/match the following/angry birds.png")
+roblox=pygame.image.load("C:/Users/User/Desktop/gaming concepts/PRO GAME DEV/match the following/roblox.png")
+minecraft=pygame.image.load("C:/Users/User/Desktop/gaming concepts/PRO GAME DEV/match the following/minecraft.png")
 
 subway_surfers = pygame.transform.scale(subway_surfers,(100,100))
 candy_crush = pygame.transform.scale(candy_crush,(100,100))
 cut_the_rope = pygame.transform.scale(cut_the_rope,(100,100))
 angry_birds = pygame.transform.scale(angry_birds,(100,100))
+roblox = pygame.transform.scale(roblox,(135,100))
+minecraft = pygame.transform.scale(minecraft,(100,100))
 
 f=pygame.font.SysFont("Comic Sans MS",36)
 
@@ -21,6 +25,8 @@ text1=f.render("Subway Surfers",True,"white")
 text2=f.render("Candy Crush",True,"white")
 text3=f.render("Cut The Rope",True,"white")
 text4=f.render("Angry Birds",True,"white")
+text5=f.render("Roblox",True,"white")
+text6=f.render("Minecraft",True,"white")
 
 gameloop = True
 
@@ -34,11 +40,15 @@ while gameloop:
         screen.blit(candy_crush,(150,5))
         screen.blit(cut_the_rope,(150,305))
         screen.blit(angry_birds,(150,105))
+        screen.blit(roblox,(135,505))
+        screen.blit(minecraft,(150,405))
 
         screen.blit(text1,(300,50))
         screen.blit(text2,(300,150))
         screen.blit(text3,(300,250))
         screen.blit(text4,(300,350))
+        screen.blit(text5,(300,450))
+        screen.blit(text6,(300,550))
 
     
         if event.type==pygame.MOUSEBUTTONDOWN:
